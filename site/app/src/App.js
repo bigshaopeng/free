461a3b2444e308d9ed1359a396c21f3df69e5c71
zhangshaopeng <1226985826@qq.com>
2019-07-21 19:54:49 +0800

    0721

--- site/app/src/App.js
+++ site/app/src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Switch, Route } from 'react-router-dom'
+import { Login } from './page/login/login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HashRouter>
+        <Switch>
+          {/* <Route exact path='/home' component={Home} /> */}
+          <Route path='/' component={Login} />
+        </Switch>
+      </HashRouter>
     );
   }
 }
