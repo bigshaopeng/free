60244c99e39b0381354d42f5b70e04dcb13c2fb3
zhangshaopeng <shaopengzhang_a2@163.com>
2019-08-02 13:54:55 +0800

    zsp

--- site/app/src/page/logout.js
+++ site/app/src/page/logout.js
@@ -27,11 +27,34 @@ const sty = {
 //     supplier: 'rgb(135, 208, 104)',
 //     manufacturer: '#ff7067',
 //     dealer: '#1890ff',
+let fileName;
+let file;
 export class Logout extends React.Component {
-    tologout = () => {
+    toLogout = () => {
         $.ajax({
             type: 'post',
             url: `http://localhost:8888/post_login`,
+            success: (data) => {
+                // this.props.
+                console.log(data)
+            },
+            error: (err) => {
+                console.log(err)
+            }
+        })
+    }
+    toUpload = () => {
+        console.log(fileName, file)
+
+        $.ajax({
+            type: 'post',
+            url: `http://localhost:8888/post_upload`,
+            data: {
+                file,
+                fileName,
+            },
+            contentType: false,
+            processData: false,
             success: (data) => {
                 // this.props.
                 console.log(data)
@@ -48,6 +71,21 @@ export class Logout extends React.Component {
             <div className={sty.body}>
                 <div>退出</div>
                 <Link to='/home'>首页</Link>
+                <form action="http://localhost:8888/post_upload" method="post" enctype="multipart/form-data">
+                    <input type="file" name="image" size="50" />
+                    <br />
+                    <input type="submit" value="上传文件" />
+                </form>
+                <br />
+                <input
+                    id='upload'
+                    type='file'
+                    onChange={(e) => {
+                        fileName = e.currentTarget.files[0]['name'];
+                        file = e.target.files;
+                    }}
+                />
+                <div onClick={this.toUpload}>提交</div>
             </div>
         </div>
     }
