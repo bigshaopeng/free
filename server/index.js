a592dda07ebc1267c23ee2228ce7d57a2b132d5e
zhangshaopeng <shaopengzhang_a2@163.com>
2019-09-11 11:32:14 +0800

    zsp

--- server/index.js
+++ server/index.js
@@ -1,36 +1,27 @@
-require('./express/express').start()
+// require('./express/express').start()
 
-// const pg = require('pg')
-// const express = require('express');
-// const BP = require('body-parser');
-// const UBP = BP.urlencoded({ extended: false });
-// const path = require('path');
-// const app = express();
-// app.use('/data', express.static('./'))
+const { Pool, Client } = require('pg')
+const express = require('express');
+const BP = require('body-parser');
+const UBP = BP.urlencoded({ extended: false });
+const path = require('path');
+const app = express();
+app.use('/data', express.static('./'))
 
 // app.get('/i.html', (req, res) => res.sendFile(__dirname + '/index.html'))
 // app.listen(8989)
 
-// var configs = `tcp://postgres:psqls@localhost:8081/Tables`
-// var clientPost = {
-//     user: "postgres",
-//     password: "psqls",
-//     database: "postgres",
-//     port: 8081,
-// };
-// // const client = ;
+const pool = new Pool({
+    user: 'postgres',
+    password: 'postgres',
+    host: '192.168.99.100',
+    port: 5432,
+    database: 'postgres',
+    sslmode: 'disable'
+})
 
-// app.get('/regist', (req, res) => {
-//     const conn = new pg.Client(configs)
-//     console.log('start', conn)
-//     conn.connect();
-//     console.log('end', conn)
-//     conn.query('SELECT * FROM aaaaaa', (err, results) => {
-//         console.log(err)
-//         console.log(results.rows)
-//     })
-//     res.end('end')
-//     // res.sendFile(path.join(__dirname, '../site/html/regist.html'))
-
-// })
-// app.listen(8888);
+pool.query('select * from account_account', (err, ress) => {
+    console.log(err, ress.rows);
+    // res.send({ "info_name": ress.rows });
+    pool.end()
+})
