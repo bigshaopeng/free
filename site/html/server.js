6297903cec251f2d0feeae07a86a7f84bd781a54
zhangshaopeng <shaopengzhang_a2@163.com>
2019-09-18 14:47:04 +0800

    zsp

--- site/html/server.js
+++ site/html/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const open = require('child_process');
 const app = express();
 
 app.use('/file', express.static('./'))
 app.listen(3000);
+open.exec('open http://localhost:3000/file/async.html')
