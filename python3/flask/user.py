5670437d6db31c16dacc590a1dea4b5333c42dba
13522648280@163.com <13522648280@163.com>
2021-08-30 17:17:20 +0800

    m

--- python3/flask/user.py
+++ python3/flask/user.py
@@ -7,18 +7,12 @@ def user(app):
     # 用户登录
     @app.route('/login', methods=['POST'])
     def login():
-        if request.method == 'POST':
-            # mysql 连接查询
-            # conn = get_connection()
-            # cursor = conn.cursor(pymysql.cursors.DictCursor)
-            # cursor.execute("select * from a_user")
-            data = sql_handle("select * from a_user")
-
-            res = {'msg': data, 'code': 200}
-            return json.dumps(res, ensure_ascii=False)
-        else:
-            res = {'msg': 'login error', 'code': 200}
-            return json.dumps(res, ensure_ascii=False)
+        # body = request.args or {}  # get
+        body = request.json or {}  # post
+        print(body)
+        data = sql_handle("select * from a_user")
+        res = {'msg': data, 'code': 200}
+        return json.dumps(res, ensure_ascii=False)
 
     # 用户退出
     # @app.route('/quit', methods=['POST'])
