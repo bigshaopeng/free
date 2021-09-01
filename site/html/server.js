c896ba67b4abb2366e3a67ba543e7db2d067d988
13522648280@163.com <13522648280@163.com>
2021-09-01 15:00:36 +0800

    m

--- site/html/server.js
+++ site/html/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 // const open = require('child_process');
 const app = express();
 
-app.use('/', express.static('./home/html/'))
+app.use('/', express.static('../'))
+// app.use('/', express.static('./home/html/'))
 app.listen(8081);
 // app.listen(3000);
 // open.exec('open http://localhost:3000/await.html')
